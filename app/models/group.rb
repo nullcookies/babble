6e5ef45e1549eefb28de98bb1c5d05720bb00961
Sergei Porfenovich <sergei.porfenovich@gmail.com>
2019-08-14 11:19:07 +0300

    issues #282 (Fix Admins can’t see a group set to `group owner` only visibility)

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class ::Group
   @@visible_group_scope = method(:visible_groups).clone
   scope :visible_groups, ->(user, order = nil, opts = {}) {
-    @@visible_group_scope.call(user, order, opts).where.not(visibility_level: 4)
+    @@visible_group_scope.call(user, order, opts)
   }
 end
